40df5c76d2942cf5c09b6d2fd01a10f53668f08e
ParamagicDev <konnor5456@gmail.com>
2021-05-01 15:33:32 -0400

    fix declaration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { Mrujs } from './mrujs'
-
-declare global {
-  interface Window {
-    mrujs?: Mrujs
-    events: Record<string, undefined | number>
-  }
-}
-
-export { Mrujs }
+export { Mrujs } from './mrujs'
