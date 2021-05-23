b194aafa22b66bb71aeb989735636732e73111e6
ParamagicDev <konnor5456@gmail.com>
2021-05-23 17:03:57 -0400

    version 15

--- lib/mrujs_rails/redirection.rb
+++ lib/mrujs_rails/redirection.rb
@@ -7,37 +7,38 @@ module MrujsRails
     end
 
     def redirect_to(url = {}, options = {})
-      # turbolinks = options.delete(:turbolinks)
+      turbolinks = options.delete(:turbolinks)
 
       super.tap do
-        if request.xhr? && !request.get?
-          visit_location_with_turbolinks(location, turbolinks)
-        elsif request.headers["Turbolinks-Referrer"]
-          store_turbolinks_location_in_session(location)
-        end
+        response.set_header("X-Xhr-Redirect", location)
+        response.set_header("TURBOLINKS-REDIRECT-ACTION", turbolinks.to_s)
+        response.set_header("TURBOLINKS-REDIRECT-LOCATION", location)
+        response.content_type = Mime[:turbolinks]
+        # if request.xhr? && !request.get?
+        #   visit_location_with_turbolinks(location, turbolinks)
+        # elsif request.headers["Turbolinks-Referrer"]
+        #   store_turbolinks_location_in_session(location)
+        # end
       end
     end
 
     private
 
-    def visit_location_with_turbolinks(location, action)
-      action = action.to_s
-      visit_action = action == "advance" ? action : "replace"
+    # def visit_location_with_turbolinks(location, action)
+    #   action = action.to_s
+    #   visit_action = action == "advance" ? action : "replace"
 
-      response.content_type = Mime[:turbolinks]
-      response.headers["X-Xhr-Redirect"] = location
-      response.headers["TURBOLINKS-REDIRECT-ACTION"] = visit_action
-      response.headers["TURBOLINKS-REDIRECT-LOCATION"] = location
-    end
+    #   response.content_type = Mime[:turbolinks]
+    # end
 
     def store_turbolinks_location_in_session(location)
       session[:_turbolinks_location] = location if session
     end
 
     def set_turbolinks_location_header_from_session
-      if session && session[:_turbolinks_location]
-        response.headers["Turbolinks-Location"] = session.delete(:_turbolinks_location)
-      end
+      return unless session && session[:_turbolinks_location]
+
+      response.set_header("Turbolinks-Location", session.delete(:_turbolinks_location))
     end
   end
 end
--- lib/mrujs_rails/version.rb
+++ lib/mrujs_rails/version.rb
@@ -1,3 +1,3 @@
 module MrujsRails
-  VERSION = "0.1.0-beta.14"
+  VERSION = "0.1.0-beta.15"
 end
