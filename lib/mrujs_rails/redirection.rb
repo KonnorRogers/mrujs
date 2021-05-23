196237beca25742fd9e28d4f7005df5aee15792b
ParamagicDev <konnor5456@gmail.com>
2021-05-23 17:11:29 -0400

    working on redirection

--- lib/mrujs_rails/redirection.rb
+++ lib/mrujs_rails/redirection.rb
@@ -10,26 +10,25 @@ module MrujsRails
       turbolinks = options.delete(:turbolinks)
 
       super.tap do
-        response.set_header("X-Xhr-Redirect", location)
-        response.set_header("TURBOLINKS-REDIRECT-ACTION", turbolinks.to_s)
-        response.set_header("TURBOLINKS-REDIRECT-LOCATION", location)
-        response.content_type = Mime[:turbolinks]
-        # if request.xhr? && !request.get?
-        #   visit_location_with_turbolinks(location, turbolinks)
-        # elsif request.headers["Turbolinks-Referrer"]
-        #   store_turbolinks_location_in_session(location)
-        # end
+        if turbolinks != false && request.xhr? && !request.get?
+          visit_location_with_turbolinks(location, turbolinks)
+        elsif request.headers["Turbolinks-Referrer"]
+          store_turbolinks_location_in_session(location)
+        end
       end
     end
 
     private
 
-    # def visit_location_with_turbolinks(location, action)
-    #   action = action.to_s
-    #   visit_action = action == "advance" ? action : "replace"
+    def visit_location_with_turbolinks(location, action)
+      action = action.to_s
+      visit_action = action == "advance" ? action : "replace"
 
-    #   response.content_type = Mime[:turbolinks]
-    # end
+      response.set_header("X-Xhr-Redirect", location)
+      response.set_header("TURBOLINKS-REDIRECT-ACTION", visit_action)
+      response.set_header("TURBOLINKS-REDIRECT-LOCATION", location)
+      response.content_type = Mime[:turbolinks]
+    end
 
     def store_turbolinks_location_in_session(location)
       session[:_turbolinks_location] = location if session
