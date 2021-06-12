678a9afbe407feb66475e95b5b7db8a74e5e926f
Konnor Rogers <konnor5456@gmail.com>
2021-06-12 01:04:35 -0400

    Feat: send method through events (#39)
    * v0.3.0-beta.11
    
    * feat: methods now go through the event chain

--- test/rails/dummy/test/system/remote_links_test.rb
+++ test/rails/dummy/test/system/remote_links_test.rb
@@ -7,4 +7,12 @@ class RemoteFormsTest < ApplicationSystemTestCase
     click_link "Remote Get Link"
     assert_current_path remote_links_path
   end
+
+  test "it should use ajax if only data-method is specifed, we assume remote" do
+    visit remote_links_path
+
+    assert_current_path remote_links_path
+    click_link "Method Get Link"
+    assert_current_path root_path
+  end
 end
