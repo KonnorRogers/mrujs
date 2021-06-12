25f219b96f54e224999259264b3613bd561d5161
ParamagicDev <konnor5456@gmail.com>
2021-06-12 18:45:55 -0400

    add some nicer workings for json

--- src/http/fetchResponse.ts
+++ src/http/fetchResponse.ts
@@ -5,6 +5,8 @@ import { expandUrl } from '../utils/url'
 export class FetchResponse {
   readonly response: Response
   private __responseHtml__!: Promise<string>
+  private __responseText__!: Promise<string>
+  private __responseJson__!: Promise<Record<string, unknown>>
 
   constructor (response: Response) {
     this.response = response
@@ -47,17 +49,34 @@ export class FetchResponse {
   }
 
   get responseText (): Promise<string> {
-    return this.response.text()
+    if (this.__responseText__ != null) return this.__responseText__
+
+    return (this.__responseText__ = this.response.text())
   }
 
   get responseHtml (): Promise<string> {
     if (this.isHtml) {
       if (this.__responseHtml__ != null) return this.__responseHtml__
 
-      return (this.__responseHtml__ = this.response.text())
-    } else {
-      return Promise.reject(this.response)
+      return (this.__responseHtml__ = this.responseText)
     }
+
+    return Promise.reject(this.response)
+  }
+
+  get responseJson (): Promise<Record<string, unknown>> {
+    if (this.isJson) {
+      if (this.__responseJson__ != null) return this.__responseJson__
+
+      return (this.__responseJson__ = this.response.json())
+    }
+
+    return Promise.reject(this.response)
+  }
+
+  // https://fetch.spec.whatwg.org/#fetch-api
+  get isJson (): boolean {
+    return Boolean(this.contentType?.toLowerCase().match(/^application\/json/))
   }
 
   getHeader (name: string): string | null {
