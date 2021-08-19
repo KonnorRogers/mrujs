0216efb0fa23a62d70fdafa19c6785a4bdc592ee
ParamagicDev <konnor5456@gmail.com>
2021-08-19 15:06:30 -0400

    add tests around content types

--- test/js/plugins/cableCar.test.ts
+++ test/js/plugins/cableCar.test.ts
@@ -7,25 +7,25 @@ import mrujs, { CableCar } from '../../../src'
 const cableCar = new CableCar(CableReady)
 
 describe('CableCar with standard config', () => {
-  beforeEach(() => {
-    mrujs.start({
-      plugins: [
-        cableCar
-      ]
-    })
-  })
-
-  afterEach(() => {
+  it('Should add CableCar to the list of plugins', () => {
+    mrujs.start({ plugins: [cableCar] })
+    assert(window.mrujs.plugins.find((plugin) => plugin.name === 'CableCar'))
     window.mrujs.stop()
   })
 
-  it('Should add CableCar to the list of plugins', () => {
-    assert(window.mrujs.plugins.find((plugin) => plugin.name === 'CableCar'))
-  })
-
   it('Should automatically add data-type and data-remote to data-cable-car elements', () => {
+    mrujs.start({ plugins: [cableCar] })
     const link = document.querySelector('#cable-car-link') as HTMLElement
     assert.equal(link.dataset.type, cableCar.mimeType)
     assert.equal(link.dataset.remote, 'true')
+    window.mrujs.stop()
+  })
+
+  it('Should return true for the given content type', () => {
+    assert(cableCar.isCableReadyResponse("application/vnd.cable-ready.json; charset='utf-8'"))
+  })
+
+  it('Should return true for the given content type', () => {
+    assert.isNotTrue(cableCar.isCableReadyResponse("app/vnd.cable-ready.json; charset='utf-8'"))
   })
 })
