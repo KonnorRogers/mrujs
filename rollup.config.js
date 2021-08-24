345852951c180ed1a5171927d1800e6a701fb8c7
ParamagicDev <konnor5456@gmail.com>
2021-08-24 04:41:29 -0400

    minor tweaks

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve from "@rollup/plugin-node-resolve"
+import typescript from "@rollup/plugin-typescript"
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        name: "mrujs",
+        file: "dist/mrujs.umd.js",
+        format: "umd",
+        sourcemap: true,
+        exports: "named"
+      },
+      {
+        file: "dist/mrujs.module.js",
+        format: "es",
+        sourcemap: true,
+      }
+    ],
+    plugins: [
+      resolve(),
+      typescript()
+    ],
+    watch: {
+      include: "src/**"
+    }
+  },
+]
