c9e2f03c6355062a8e7ef1c2d6bdf8c011c9a66f
Konnor Rogers <konnor5456@gmail.com>
2021-09-01 21:52:01 -0400

    remote GET links now navigate (#111)

--- test/rails/dummy/app/controllers/static_controller.rb
+++ test/rails/dummy/app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
   def index
   end
+
+  def no_content
+  end
 end
--- test/rails/dummy/config/routes.rb
+++ test/rails/dummy/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "static#index"
 
+  post "/no_content", to: "static#no_content"
+
   resources :posts
   resources :remote_links
 end
