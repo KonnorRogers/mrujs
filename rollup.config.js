425f9532eba5ee2a82fc9d44fc884acc4dbf0a0a
ParamagicDev <konnor5456@gmail.com>
2021-09-07 01:48:56 -0400

    back to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,35 @@
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "@rollup/plugin-typescript"
 
-// import analyze from 'rollup-plugin-analyzer'
 import { terser } from "rollup-plugin-terser";
 import { brotliCompressSync } from 'zlib'
 import gzipPlugin from 'rollup-plugin-gzip'
+
+function basePlugins(tsconfig = "tsconfig.json") {
+  return [
+    resolve(),
+    typescript({ tsconfig }),
+  ]
+}
+
+function compressionPlugins(tsconfig = "tsconfig.json") {
+  return [
+    ...basePlugins(tsconfig),
+    terser({
+      compress: {
+        passes: 10
+      }
+    }),
+    // GZIP compression as .gz files
+    gzipPlugin(),
+    // Brotil compression as .br files
+    gzipPlugin({
+        customCompression: content =>
+            brotliCompressSync(Buffer.from(content)),
+        fileName: '.br',
+    }),
+  ]
+}
 
 export default [
   {
@@ -23,11 +48,31 @@ export default [
         sourcemap: true,
       }
     ],
-    plugins: [
-      resolve(),
-      typescript(),
-    ]
+    plugins: basePlugins()
   },
+
+  // Plugins
+  {
+    input: "plugins/src/index.ts",
+    output: [
+      {
+        name: "mrujs",
+        file: "plugins/dist/mrujs.umd.js",
+        format: "umd",
+        sourcemap: true,
+        exports: "named"
+      },
+      {
+        file: "plugins/dist/mrujs.module.js",
+        format: "es",
+        sourcemap: true,
+      }
+    ],
+    plugins: basePlugins("tsconfig-plugins.json")
+  },
+
+
+  // Compressed
   {
     input: "src/index.ts",
     output: [
@@ -44,26 +89,25 @@ export default [
         sourcemap: true,
       }
     ],
-    plugins: [
-      resolve(),
-      typescript(),
-      terser({
-        compress: {
-          passes: 10
-        }
-      }),
-      // GZIP compression as .gz files
-      gzipPlugin(),
-      // Brotil compression as .br files
-      gzipPlugin({
-          customCompression: content =>
-              brotliCompressSync(Buffer.from(content)),
-          fileName: '.br',
-      }),
-      // analyze()
+    plugins: compressionPlugins(),
+  },
+
+  {
+    input: "plugins/src/index.ts",
+    output: [
+      {
+        name: "mrujs",
+        file: "plugins/dist/mrujs.umd.min.js",
+        format: "umd",
+        sourcemap: true,
+        exports: "named"
+      },
+      {
+        file: "plugins/dist/mrujs.module.min.js",
+        format: "es",
+        sourcemap: true,
+      }
     ],
-    watch: {
-      include: "src/**"
-    }
+    plugins: compressionPlugins("tsconfig-plugins.json")
   },
 ]
