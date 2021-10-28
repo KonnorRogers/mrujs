d7579c9a3629f72084cf4a9d333001d6344fafba
Ricky Chilcott <ricky@rakefire.io>
2021-10-27 23:34:02 -0400

    Pass value of select elements (#160)
    * Pass value of select elements
    
    * Because element.value could be undefined, switch back to null
    
    * fix: use just element.value
    
    * Update methodSubmission.ts
    
    * Correct test to ensure submission when value isn't explicitly set
    
    Co-authored-by: Konnor Rogers <konnor5456@gmail.com>

--- test/js/methodSubmission.test.ts
+++ test/js/methodSubmission.test.ts
@@ -14,6 +14,42 @@ describe('MethodSubmission', () => {
     assert.equal('duh', body.get('blah'))
   })
 
+  it('Should append Select name / value to the formData object', async () => {
+    const el: HTMLElement = await fixture(html`
+      <select name="blah" data-method="post" data-remote="true" data-url="https://blah.com">
+        <option value="haha" selected>Ha</option>
+        <option value="nono">No</option>
+        <option value="yaya">Yay</option>
+      </select>`)
+
+    const submission = MethodSubmission(el)
+
+    const body = submission.fetchRequest.body as URLSearchParams
+    assert.equal('haha', body.get('blah'))
+  })
+
+  it('Should not append name / value to the formData object when no name', async () => {
+    const el: HTMLElement = await fixture(html`
+      <button value="duh" data-method="post" data-remote="true" data-url="https://blah.com">
+      </button>`)
+
+    const submission = MethodSubmission(el)
+
+    const body = submission.fetchRequest.body as URLSearchParams
+    assert.equal(0, Array.from(body.keys()).length)
+  })
+
+  it('Should append name / value to the formData object when empty string value', async () => {
+    const el: HTMLElement = await fixture(html`
+      <button name="haha" data-method="post" data-remote="true" data-url="https://blah.com">
+      </button>`)
+
+    const submission = MethodSubmission(el)
+
+    const body = submission.fetchRequest.body as URLSearchParams
+    assert.equal('', body.get('haha'))
+  })
+
   it('Should should account for extra JSON params', async () => {
     const el: HTMLElement = await fixture(html`
       <button name="blah" value="duh" data-method="post" data-remote="true" data-url="https://blah.com" data-params='{"myKey": "myValue", "myKey2": "myValue2"}'>
