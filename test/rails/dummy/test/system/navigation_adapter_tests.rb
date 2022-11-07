7710dad5d6a399056595fcbe4b93d0b25d5d79c5
Konnor Rogers <konnor5456@gmail.com>
2022-11-07 17:35:14 -0500

    Update navigation_adapter_tests.rb

--- test/rails/dummy/test/system/navigation_adapter_tests.rb
+++ test/rails/dummy/test/system/navigation_adapter_tests.rb
@@ -5,7 +5,7 @@ module NavigationAdapterTests
     test "Should add /posts to the snapshot cache" do
       pre_fetch_count = evaluate_script("window.mrujs.navigationAdapter.snapshotCache.keys.length")
 
-      execute_script("window.mrujs.prefetch('/posts')")
+      execute_script("window.mrujs.navigationAdapter.cacheHTML({ url: '/posts', html: '<div></div>' })")
 
       post_fetch_count = evaluate_script("window.mrujs.navigationAdapter.snapshotCache.keys.length")
 
